87ea0eec98ea77752c4c4eaf27f0ba4b19acd08b
Wardog <wardog@wrdg.net>
2022-10-19 21:03:16 -0500

    Update VersionExtensions.cs

--- KuruExtract/Extensions/VersionExtensions.cs
+++ KuruExtract/Extensions/VersionExtensions.cs
@@ -4,6 +4,11 @@ internal static class VersionExtensions
 {
     public static Version Normalize(this Version version)
     {
-        return new Version(Math.Max(version.Major, 0), Math.Max(version.Minor, 0), Math.Max(version.Build, 0), Math.Max(version.Revision, 0));
+        return new Version(
+            Math.Max(version.Major, 0),
+            Math.Max(version.Minor, 0),
+            Math.Max(version.Build, 0),
+            Math.Max(version.Revision, 0)
+        );
     }
 }
