8e662ba868bce7f550ecad10200e54e95269006c
Wardog <wardog@wrdg.net>
2024-04-28 19:21:58 -0500

    Fix versioning for later .NET versions

--- KuruExtract/Constants.cs
+++ KuruExtract/Constants.cs
@@ -1,29 +1,18 @@
 ﻿using Spectre.Console;
 using Spectre.Console.Rendering;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace KuruExtract;
 internal static class Constants
 {
-    public static IRenderable Header
-    {
-        get
-        {
-            return new Panel(new Markup("\nExtracts game content for DayZ\nby Wardog & Ryann\n").Centered())
-                .Header($"ExtractDayZ v{Version}", Justify.Center)
-                .SafeBorder()
-                .Border(BoxBorder.Heavy)
-                .Expand();
-        }
-    }
+    public static IRenderable Header =>
+        new Panel(new Markup("\nExtracts game content for DayZ\nby Wardog\n").Centered())
+            .Header($"ExtractDayZ v{Version}", Justify.Center)
+            .SafeBorder()
+            .Border(BoxBorder.Heavy)
+            .Expand();
 
-    public static string Version
-    {
-        get
-        {
-            return Assembly.GetExecutingAssembly()?
-                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                .InformationalVersion ?? "0.0.0";
-        }
-    }
+    public static string Version =>
+        FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion ?? "0.0.0";
 }
