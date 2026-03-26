dad54bd9a23eaf5bd53c35e32b7a4eecf4506423
Wardog <wardog@wrdg.dev>
2026-03-26 16:20:10 -0500

    Move param indent logic

--- src/KuruExtract/RV/Config/Params/ParamEntry.cs
+++ src/KuruExtract/RV/Config/Params/ParamEntry.cs
@@ -5,6 +5,13 @@ internal abstract class ParamEntry
 {
     public string Name { get; protected set; } = string.Empty;
 
+    private static readonly string[] IndentStrings = Enumerable.Range(0, 16)
+        .Select(i => new string(' ', i * 4))
+        .ToArray();
+
+    protected static string Indent(int level) =>
+        (uint)level < (uint)IndentStrings.Length ? IndentStrings[level] : new string(' ', level * 4);
+
     public static ParamEntry ReadParamEntry(RVBinaryReader input)
     {
         var entryType = (EntryType)input.ReadByte();
