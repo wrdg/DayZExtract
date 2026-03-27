a287ee43e74c13fe72f7c59d8bd2e7f61951a516
Wardog <wardog@wrdg.dev>
2026-03-26 21:35:36 -0500

    Roll version

--- src/KuruExtract/Constants.cs
+++ src/KuruExtract/Constants.cs
@@ -1,23 +1,15 @@
 ﻿using Spectre.Console;
 using Spectre.Console.Rendering;
-using System.Reflection;
 
 namespace KuruExtract;
 internal static class Constants
 {
+    public const string Version = "1.1.0";
+
     public static IRenderable Header =>
         new Panel(new Markup("\nExtracts game content for DayZ\nby Wardog\n").Centered())
             .Header($"DayZExtract v{Version}", Justify.Center)
             .SafeBorder()
             .Border(BoxBorder.Heavy)
             .Expand();
-
-    public static string Version
-    {
-        get
-        {
-            var version = Assembly.GetExecutingAssembly()?.GetName()?.Version;
-            return version != null ? $"{version.Major}.{version.Minor}.{version.Build}" : "0.0.0";
-        }
-    }
 }
