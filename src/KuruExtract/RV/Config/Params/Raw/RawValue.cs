1dc57f392f35b5930bfafe653c0d0b47a264a589
Wardog <wardog@wrdg.dev>
2026-03-31 02:52:46 -0500

    Avoid boxing raw config values

--- src/KuruExtract/RV/Config/Params/Raw/RawValue.cs
+++ src/KuruExtract/RV/Config/Params/Raw/RawValue.cs
@@ -1,35 +1,46 @@
-﻿using KuruExtract.RV.IO;
+using KuruExtract.RV.IO;
 using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace KuruExtract.RV.Config;
 internal sealed class RawValue
 {
+    [StructLayout(LayoutKind.Explicit)]
+    private struct NumericUnion
+    {
+        [FieldOffset(0)] public int Int;
+        [FieldOffset(0)] public long Int64;
+        [FieldOffset(0)] public float Float;
+    }
+
     public ValueType Type { get; set; }
 
-    public object Value { get; }
+    private readonly string? _string;
+    private readonly RawArray? _array;
+    private NumericUnion _numeric;
 
     public RawValue(string v)
     {
         Type = ValueType.String;
-        Value = v;
+        _string = v;
     }
 
     public RawValue(int v)
     {
         Type = ValueType.Int;
-        Value = v;
+        _numeric.Int = v;
     }
 
     public RawValue(long v)
     {
         Type = ValueType.Int64;
-        Value = v;
+        _numeric.Int64 = v;
     }
 
     public RawValue(float v)
     {
         Type = ValueType.Float;
-        Value = v;
+        _numeric.Float = v;
     }
 
     public RawValue(RVBinaryReader input) : this(input, (ValueType)input.ReadByte()) { }
@@ -37,31 +48,45 @@ internal sealed class RawValue
     public RawValue(RVBinaryReader input, ValueType type)
     {
         Type = type;
-        Value = Type switch
+        switch (type)
         {
-            ValueType.Expression or ValueType.String => input.ReadAsciiz(),
-            ValueType.Float => input.ReadSingle(),
-            ValueType.Int => input.ReadInt32(),
-            ValueType.Int64 => input.ReadInt64(),
-            ValueType.Array => new RawArray(input),
-            _ => throw new ArgumentException(),
-        };
+            case ValueType.Expression or ValueType.String:
+                _string = input.ReadAsciiz();
+                break;
+            case ValueType.Float:
+                _numeric.Float = input.ReadSingle();
+                break;
+            case ValueType.Int:
+                _numeric.Int = input.ReadInt32();
+                break;
+            case ValueType.Int64:
+                _numeric.Int64 = input.ReadInt64();
+                break;
+            case ValueType.Array:
+                _array = new RawArray(input);
+                break;
+            default:
+                throw new ArgumentException($"Unexpected value type: {type}");
+        }
     }
 
     public override string? ToString()
     {
         return Type switch
         {
-            ValueType.Expression or ValueType.String => FormatString((string)Value),
-            ValueType.Float => ((float)Value).ToString(CultureInfo.InvariantCulture),
-            _ => Value.ToString()
+            ValueType.Expression or ValueType.String => FormatString(_string!),
+            ValueType.Float => _numeric.Float.ToString(CultureInfo.InvariantCulture),
+            ValueType.Int => _numeric.Int.ToString(),
+            ValueType.Int64 => _numeric.Int64.ToString(),
+            ValueType.Array => _array!.ToString(),
+            _ => null
         };
     }
 
     public bool WasEscaped => Type switch
     {
-        ValueType.String or ValueType.Expression => ((string)Value).Contains('"'),
-        ValueType.Array => ((RawArray)Value).Entries.Any(e => e.WasEscaped),
+        ValueType.String or ValueType.Expression => _string!.Contains('"'),
+        ValueType.Array => _array!.Entries.Any(e => e.WasEscaped),
         _ => false
     };
 
@@ -70,6 +95,14 @@ internal sealed class RawValue
 
     internal T Get<T>()
     {
-        return (T)Convert.ChangeType(Value, typeof(T));
+        object boxed = Type switch
+        {
+            ValueType.Expression or ValueType.String => _string!,
+            ValueType.Float => _numeric.Float,
+            ValueType.Int => _numeric.Int,
+            ValueType.Int64 => _numeric.Int64,
+            _ => _array!
+        };
+        return (T)Convert.ChangeType(boxed, typeof(T));
     }
 }
